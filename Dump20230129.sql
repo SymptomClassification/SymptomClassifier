990db4c7e88c49457d72056a93e20a243710cab4
ahmet <a.duser@lancaster.ac.uk>
2023-01-30 14:23:15 +0100

    chapter id changed to auto increment

--- Dump20230129.sql
+++ Dump20230129.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `chapter`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `chapter` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
