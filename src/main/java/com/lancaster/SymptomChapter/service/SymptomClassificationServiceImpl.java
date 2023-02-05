f092ad2655405f402c06127f351d1277c5814372
ahmet <a.duser@lancaster.ac.uk>
2023-02-05 22:18:32 +0100

    test webhook

--- src/main/java/com/lancaster/SymptomChapter/service/SymptomClassificationServiceImpl.java
+++ src/main/java/com/lancaster/SymptomChapter/service/SymptomClassificationServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-// test for git web hook
+
 @Service
 public class SymptomClassificationServiceImpl implements SymptomClassificationService {
 
