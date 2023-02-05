9695966738446e2016ac325d1cb9998fa5173b32
ahmet <a.duser@lancaster.ac.uk>
2023-02-05 20:33:15 +0100

    test cases added

---
+++ src/test/java/com/lancaster/SymptomChapter/ClassifiedSymptomControllerTest.java
@@ -0,0 +1,82 @@
+package com.lancaster.SymptomChapter;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lancaster.SymptomChapter.controller.SymptomClassificationController;
+import com.lancaster.SymptomChapter.model.ClassifiedSymptom;
+import com.lancaster.SymptomChapter.service.SymptomClassificationService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(MockitoJUnitRunner.class)
+@WebMvcTest(controllers = SymptomClassificationController.class)
+public class ClassifiedSymptomControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private SymptomClassificationController classifiedSymptomController;
+
+    @Mock
+    private SymptomClassificationService service;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(classifiedSymptomController).build();
+    }
+
+    @Test
+    public void testFindClassifiedSymptomWithName() throws Exception {
+        ClassifiedSymptom symptom = new ClassifiedSymptom(4, "symptom2.1", 2, 2, 2);
+        given(service.fetchClassifiedSymptomWithName("symptom2.1")).willReturn(symptom);
+        mockMvc.perform(get("/api/v1/classifiedSymptoms/findClassifiedSymptomWithName/{name}", "symptom2.1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUpdateClassifiedSymptom() throws Exception {
+
+        ClassifiedSymptom symptom = new ClassifiedSymptom(1, "Fever", 1, 2, 3);
+
+        ClassifiedSymptom mockClassifiedSymptom = new ClassifiedSymptom(1, "Fever", 1, 2, 3);
+
+        mockMvc.perform(put("/api/v1/classifiedSymptoms/updateClassifiedSymptom/{name}", "Fever")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(symptom)))
+                .andExpect(status().isOk());
+//                .andExpect(jsonPath("$.id", is(1)))
+//                .andExpect(jsonPath("$.name", is("Fever")))
+//                .andExpect(jsonPath("$.chapterId", is(1)))
+//                .andExpect(jsonPath("$.subchapterId", is(2)))
+//                .andExpect(jsonPath("$.secondsubId", is(3)));
+
+//        verify(service, times(1)).updateClassifiedSymptom(symptom, "Fever");
+//        verifyNoMoreInteractions(service);
+
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/lancaster/SymptomChapter/ClassifiedSymptomRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.lancaster.SymptomChapter;
+
+import com.lancaster.SymptomChapter.model.ClassifiedSymptom;
+import com.lancaster.SymptomChapter.repository.SymptomRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DriverManager.class)
+public class ClassifiedSymptomRepositoryTest {
+
+    @Mock
+    private Connection mockConnection;
+
+    @Mock
+    private PreparedStatement mockPreparedStatement;
+
+    @InjectMocks
+    private SymptomRepository classifiedSymptomRepository;
+
+    @Test
+    public void testSaveSymptom() throws SQLException {
+        PowerMockito.mockStatic(DriverManager.class);
+        PowerMockito.when(DriverManager.getConnection(anyString())).thenReturn(mockConnection);
+
+        ClassifiedSymptom classifiedSymptom = new ClassifiedSymptom(1, "test", 1, 2, 3);
+
+        when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
+        when(mockPreparedStatement.executeUpdate()).thenReturn(1);
+
+
+        ClassifiedSymptom savedSymptom = classifiedSymptomRepository.saveSymptom(classifiedSymptom);
+
+        verify(mockConnection).prepareStatement("INSERT INTO classifiedsymptom (name, chapterId, subchapterId, secondsubId) " +
+                " VALUES (?, ?, ?, ?)");
+        verify(mockPreparedStatement).setString(1, "test");
+        verify(mockPreparedStatement).setInt(2, 1);
+        verify(mockPreparedStatement).setInt(3, 2);
+        verify(mockPreparedStatement).setInt(4, 3);
+        verify(mockPreparedStatement).executeUpdate();
+
+        assertEquals(classifiedSymptom, savedSymptom);
+    }
+
+
+}
