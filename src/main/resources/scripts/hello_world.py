d8e3267345e1d48f6381bdef263ffbf1cd321546
ahmet <a.duser@lancaster.ac.uk>
2023-02-15 21:23:24 +0100

    hello world test is hitting the /chapters endpoint

--- src/main/resources/scripts/hello_world.py
+++ src/main/resources/scripts/hello_world.py
@@ -1,8 +1,16 @@
 import sys
+import urllib.request
+import json
 
 def main(argument):
+    url_chapters = 'http://dagere.comiles.eu:8090/chapters'
+
+    with urllib.request.urlopen(url_chapters) as response:
+        if response.status == 200:
+            json_data = response.read().decode('utf-8')
+            chapters = json.loads(json_data)
     my_list = ["Hello World", argument]
-    print(my_list)
+    print(chapters)
 
 if __name__ == '__main__':
     argument = sys.argv[1:] # Replace this with the argument you want to pass
