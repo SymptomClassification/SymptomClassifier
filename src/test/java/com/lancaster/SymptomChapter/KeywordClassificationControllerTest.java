2c50f616f248326c40479c22652108cf5315099c
ahmet <a.duser@lancaster.ac.uk>
2023-02-22 12:28:37 +0100

    classifySymptom test case is added

--- src/test/java/com/lancaster/SymptomChapter/KeywordClassificationControllerTest.java
+++ src/test/java/com/lancaster/SymptomChapter/KeywordClassificationControllerTest.java
@@ -2,9 +2,9 @@ package com.lancaster.SymptomChapter;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.lancaster.SymptomChapter.controller.SymptomClassificationController;
+import com.lancaster.SymptomChapter.controller.KeywordClassificationController;
 import com.lancaster.SymptomChapter.model.ClassifiedSymptom;
-import com.lancaster.SymptomChapter.service.SymptomClassificationService;
+import com.lancaster.SymptomChapter.service.KeywordClassificationService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,25 +16,35 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.mockito.Mockito.verify;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.mockito.BDDMockito.given;
+import static org.powermock.api.mockito.PowerMockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 
 @RunWith(MockitoJUnitRunner.class)
-@WebMvcTest(controllers = SymptomClassificationController.class)
-public class SymptomClassificationControllerTest {
+@WebMvcTest(controllers = KeywordClassificationController.class)
+public class KeywordClassificationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @InjectMocks
-    private SymptomClassificationController classifiedSymptomController;
+    private KeywordClassificationController classifiedSymptomController;
 
     @Mock
-    private SymptomClassificationService service;
+    private KeywordClassificationService service;
 
     @Before
     public void setup() {
@@ -69,6 +79,28 @@ public class SymptomClassificationControllerTest {
 //        verify(service, times(1)).updateClassifiedSymptom(symptom, "Fever");
 //        verifyNoMoreInteractions(service);
 
+    }
+
+    @Test
+    public void testClassifySymptom() throws Exception {
+        String name = "someName";
+
+        // Mock the service response
+        Map<String, String> result = new HashMap<>();
+        result.put("chapterName", "head");
+        result.put("subchapterName", "General");
+        List<Map<String, String>> responseList = Arrays.asList(result);
+        when(service.classifySymptom(name)).thenReturn(responseList);
+
+        // Perform the request
+        mockMvc.perform(get("/classifiedSymptoms/classifySymptom/{name}", name)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].chapterName").value("head"))
+                .andExpect(jsonPath("$[0].subchapterName").value("General"));
+
+        // Verify the service was called with the correct argument
+        verify(service).classifySymptom(name);
     }
 
     public static String asJsonString(final Object obj) {
